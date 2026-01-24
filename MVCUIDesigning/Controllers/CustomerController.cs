fa27fd6d619ba799aa6a73d29c3ec7009d6390b1
nimesh607 <nuvaishnav607@gmail.com>
2026-01-24 19:52:51 +0530

    Implemented Partial view

---
+++ MVCUIDesigning/Controllers/CustomerController.cs
@@ -0,0 +1,89 @@
+﻿using MVCUIDesigning.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCUIDesigning.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Customer c1 = new Customer
+        {
+            Custid = 1001,
+            Name = "Peter",
+            Account = "Savings",
+            Balance = 50000.00,
+            City = "Delhi",
+            Status = true,
+
+        };
+        Customer c2 = new Customer
+        {
+            Custid = 1002,
+            Name = "Kevin",
+            Account = "Current",
+            Balance = 50000.00,
+            City = "Kolkata",
+            Status = true,
+
+        };
+        Customer c3 = new Customer
+        {
+            Custid = 1003,
+            Name = "Sandra",
+            Account = "Demat",
+            Balance = 50000.00,
+            City = "Mumabi",
+            Status = true,
+
+        };
+        Customer c4 = new Customer
+        {
+            Custid = 1004,
+            Name = "Williams",
+            Account = "Savings",
+            Balance = 50000.00,
+            City = "Chennai",
+            Status = true,
+
+        };
+        Customer c5 = new Customer
+        {
+            Custid = 1005,
+            Name = "John",
+            Account = "Demat",
+            Balance = 50000.00,
+            City = "Bengaluru",
+            Status = true,
+
+        };
+        Customer c6 = new Customer
+        {
+            Custid = 1006,
+            Name = "Bill",
+            Account = "Current",
+            Balance = 50000.00,
+            City = "Hyderabad",
+            Status = true,
+
+        };
+
+        public ViewResult DisplayCustomers()
+       {
+            //this is without partial view           
+            List<Customer> customers = new List<Customer> { c1, c2, c3, c4, c5, c6 };
+            return View(customers);
+        }
+
+        public ViewResult DisplayCustomersusingpartialview()
+        {
+            //this is with partial view
+            List<Customer> customers = new List<Customer> { c1, c2, c3, c4, c5, c6 };
+            return View(customers);
+        }
+
+
+    }
+}
--- MVCUIDesigning/Controllers/StudentController.cs
+++ MVCUIDesigning/Controllers/StudentController.cs
@@ -31,5 +31,17 @@ namespace MVCUIDesigning.Controllers
         {
             return View("DisplayStudentST", student);
         }
+
+        /*
+         * A Partial View is also a View, but this can be used in other Views, i.e., they provide re-usability. Creating a Partial View is also just like creating a normal View and the extension of the Partial View also will be “.cshtml” only, but in a Partial View we don’t have any HTML tag’s like “<head>” and “<body>” by default. Partial Views are generally stored in the “Shared” folder of “Views” folder because they are consumed by multiple Views in the application, and it is suggested (optional) to prefix Partial View names with “underscore (_)” to differentiate them from other Views.
+         * Consume Partial View in a View:
+         * @Html.Partial(string PartialViewName) => MvcHtmlString
+           @{ Html.RenderPartial(string PartialViewName); } => void
+           @{ Html.RenderAction(string ActionMethodName); } => void
+         */
+        public PartialViewResult Header()
+        {
+            return PartialView("_Header");
+        }
     }
 }
